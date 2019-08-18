a6d3244961fded2cb3f6f07b8a821d915567d4a2
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-08-19 04:09:23 +0700

    change initialRouteName

--- GoogleNews/navigation/MainTabNavigator.js
+++ GoogleNews/navigation/MainTabNavigator.js
@@ -24,7 +24,7 @@ let routeConfig = {
 }
 
 let drawerNavigatorConfig = {
-  initialRouteName: "Search"
+  initialRouteName: "Top"
 }
 
 const tabNavigator = createDrawerNavigator(routeConfig, drawerNavigatorConfig);
